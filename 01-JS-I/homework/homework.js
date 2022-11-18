67426b9440a9a3c916b16f0e289d6c1d652d5f62
dryer10 <sidox_106@hotmail.com>
2022-11-18 10:36:41 -0500

    Agregado a Repositorio

---
+++ 01-JS-I/homework/homework.js
@@ -0,0 +1,31 @@
+// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = null;
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = null;
+
+// Crea una variable booleana:
+const nuevoBool = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = null;
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  nuevaString,
+  nuevoNum,
+  nuevoBool,
+  nuevaResta,
+  nuevaMultiplicacion,
+  nuevoModulo,
+};
